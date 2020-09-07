c0e1d5570087d4ddfb22000821dea439dae6df40
Akhil Tamatam <akhilreddy.tamatam@gmail.com>
2020-09-07 16:50:29 +0530

    first commit

---
+++ src/main/java/javalclasses/class1.java
@@ -0,0 +1,9 @@
+package javalclasses;
+
+public class class1 {
+
+	
+		public int add(int num1,int num2) {
+			return num1+num2;
+		}
+}
---
+++ src/test/java/javalclasses/test1.java
@@ -0,0 +1,15 @@
+package javalclasses;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class test1 {
+
+	@Test
+	public void test() {
+		class1 c=new class1();
+		assertEquals(5, c.add(1, 3));
+	}
+
+}
